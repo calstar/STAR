df99c65027a669a4dae3ce9dc08b7e093faccfd5
Leon Shams-schaal <leon@wifi-10-41-116-202.wifi.berkeley.edu>
2025-10-16 20:44:17 -0700

    Outline

---
+++ utl/matrix_alg.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+double tranpose()
+
+double ones()
+
+double eye()
+
+double matmul()
+
+double abs() {
+    // In place modify each element to its absolute value
+}
+
+double inv
+
+double l2_norm()
+
+double matrix_sum()
+
+double matrix_trace()
+
+double decompose (chelesky, plu, svd, orthonormal)
+
+double determinant()
+
+double is_PSD
+
+double is_PD
+
+double is_symmetric():
+
+double is_singular
+
+double is_diagonal()
+
+double inner_product()
+
+double cross_product
+
+double matrix_derivative
+
+double least_squares_solver(y, X, beta)
+
+void print_matrix()
