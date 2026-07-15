00fa37b6c0d38751fc0b48851a622e0ac484184c
swissskimmilk <aidanrickert@berkeley.edu>
2026-07-15 13:26:33 -0700

    Dead code cleanup, improved CI catching of bad syntax and practices

--- EngineDesign/engine/optimizer/__init__.py
+++ EngineDesign/engine/optimizer/__init__.py
@@ -5,10 +5,9 @@ This package contains the modular layers of the optimization pipeline:
 Modules:
 - helpers.py: Pressure curve generation and variable conversion
 - layers/layer1_static_optimization.py: Layer 1 geometry optimization
-- layers/layer2_pressure.py: Layer 2 pressure curve optimization  
+- layers/layer2_pressure.py: Layer 2 pressure curve optimization
 - layers/layer3_thermal_protection.py: Layer 3 thermal protection sizing
 - layers/layer4_flight_simulation.py: Layer 4 flight simulation
-- display_results.py: Plotting and visualization functions
 - copv_flight_helpers.py: COPV and flight simulation utilities
 - utils.py: Parameter extraction and misc utilities
 
@@ -23,9 +22,6 @@ Usage:
         run_layer2_pressure,
         run_layer3_thermal_protection,
         run_layer4_flight_simulation,
-        # Display
-        plot_pressure_curves,
-        plot_optimization_convergence,
         # Utilities
         extract_all_parameters,
         calculate_copv_pressure_curve,
@@ -61,16 +57,6 @@ from engine.optimizer.layers.layer4_flight_simulation import (
     run_layer4_flight_simulation,
 )
 
-# Display functions
-from engine.optimizer.display_results import (
-    plot_pressure_curves,
-    plot_copv_pressure,
-    plot_flight_trajectory,
-    plot_optimization_convergence,
-    plot_time_varying_results,
-    plot_layer1_parameterization_history,
-)
-
 # COPV and flight helpers
 from engine.optimizer.copv_flight_helpers import (
     calculate_copv_pressure_curve,
@@ -80,11 +66,6 @@ from engine.optimizer.copv_flight_helpers import (
 # Utilities
 from engine.optimizer.utils import (
     extract_all_parameters,
-)
-
-# Main optimizer
-from engine.optimizer.main_optimizer import (
-    run_full_engine_optimization_with_flight_sim,
 )
 
 __all__ = [
@@ -101,19 +82,10 @@ __all__ = [
     'run_layer3_thermal_protection',
     # Layer 4
     'run_layer4_flight_simulation',
-    # Display functions
-    'plot_pressure_curves',
-    'plot_copv_pressure',
-    'plot_flight_trajectory',
-    'plot_optimization_convergence',
-    'plot_time_varying_results',
-    'plot_layer1_parameterization_history',
     # COPV and flight
     'calculate_copv_pressure_curve',
     'run_flight_simulation',
     # Utilities
     'extract_all_parameters',
-    # Main optimizer
-    'run_full_engine_optimization_with_flight_sim',
 ]
 
